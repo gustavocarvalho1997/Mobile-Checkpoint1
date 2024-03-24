048a506114953e8c621a024fac0cd67564f0c421
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-24 19:32:04 -0300

    implementada tela para coletar informações do cliente

---
+++ src/app/index.jsx
@@ -0,0 +1,44 @@
+import { Text, View, TextInput } from 'react-native'
+import { Link } from 'expo-router';
+import React from 'react'
+import styles from './style/style';
+import cliente from './utils/cliente';
+
+
+export default function Index() {
+
+  const [nome, setNome] = React.useState('')
+  const [cpf, setCpf] = React.useState('')
+  const [telefone, setTelefone] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [dataNascimento, setDataNascimento] = React.useState('')
+
+  // salvar no objeto cliente
+  const salvar = () => {
+    cliente.nome = nome;
+    cliente.cpf = cpf;
+    cliente.telefone = telefone;
+    cliente.email = email;
+    cliente.dataNascimento = dataNascimento;
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Insira suas informações</Text>
+      <Text style={styles.label}>Nome</Text>
+      <TextInput style={styles.input} placeholder="Nome" onChangeText={(e) => setNome(e)} />
+      <Text style={styles.label}>CPF</Text>
+      <TextInput style={styles.input} placeholder="CPF" onChangeText={(e) => setCpf(e)} />
+      <Text style={styles.label}>Telefone</Text>
+      <TextInput style={styles.input} placeholder="Telefone" onChangeText={(e) => setTelefone(e)}/>
+      <Text style={styles.label}>Email</Text>
+      <TextInput style={styles.input} placeholder="Email" onChangeText={(e) => setEmail(e)}/>
+      <Text style={styles.label}>Data de Nascimento</Text>
+      <TextInput style={styles.input} placeholder="Data de Nascimento" onChangeText={(e) => setDataNascimento(e)}/>
+      <Link href={'/dadosFinanciamento'} style={styles.button} onPress={salvar}>Próximo</Link>
+    </View>
+  )
+}
+
